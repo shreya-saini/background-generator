f22dab94bfc37565c27449ea2f00fcfca6b695bf
shreya-saini <101948640+shreya-saini@users.noreply.github.com>
2022-03-20 01:21:23 +0530

    Add files via upload

---
+++ background generator/script.js
@@ -0,0 +1,23 @@
+
+
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var h3 = document.querySelector("h3");
+var body = document.getElementById('gradient')
+
+console.log("color2.value: ------", color2);
+console.log("color1.value: ------", color1);
+
+
+function setGradient(e) {
+    body.style.background = "linear-gradient(to right ,"
+    +color1.value
+    +", "
+    + color2.value
+    +")";
+
+    h3.textContent = body.style.background ;
+}
+
+color1.addEventListener('input',setGradient);
+color2.addEventListener("input",setGradient);
